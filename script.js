3465afc6e6eeb11a71e38ee294c954ecb05147de
Abdulbosit-web <abdulbosit.web@gmail.com>
2024-08-21 10:38:17 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    const offset = -currentSlide * 100;
+    document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Инициализация первого слайда
+showSlide(currentSlide);
